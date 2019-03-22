855a737864d397656ab3dbbe7ed25180678be4d3
Mathew Robinson <chasinglogic@gmail.com>
2019-03-22 11:06:11 -0400

    Make config a class for greater flexibility

---
+++ src/task_forge/cli/config.py
@@ -0,0 +1,103 @@
+"""Configuration class definition."""
+
+import os
+import sys
+import toml
+
+from task_forge.lists import InvalidConfigError
+from task_forge.lists.load import get_list
+
+CONFIG_FILES = [
+    'taskforge.toml',
+    os.path.join(os.getenv('HOME', ''), '.taskforge.d', 'config.toml'),
+    '/etc/taskforge/config.toml'
+]
+
+
+def default_list_config():
+    """Return the default list configuration."""
+    return {'name': 'sqlite', 'config': {'directory': '~/.taskforge.d'}}
+
+
+def default_server_config():
+    """Return the default configuration for a server."""
+    if sys.platform == 'win32':
+        return {
+            'host': 'localhost',
+            'port': 8080,
+            # 'secret_file': '~/.taskforge.d/server_secret'
+        }
+
+    return {
+        # By default use a unix socket not a network socket
+        'unix_socket': '/var/run/user/{}/taskforge.sock'.format(os.getuid()),
+        # Only used with network communication
+        # 'secret_file': '~/.taskforge.d/server_secret'
+    }
+
+
+class Config:
+    """Configuration object for Taskforge CLI and taskforged."""
+
+    def __init__(self, general=None, list=None, server=None):
+        self.general = general if general is not None else {}
+        self.list = list if list is not None else default_list_config()
+        self.server = server if server is not None else default_server_config()
+        self.list_impl = None
+        self.path = None
+
+    @staticmethod
+    def load(path=None):
+        """Load the config file from path or from the default config file paths."""
+        cfg = Config()
+
+        paths = CONFIG_FILES
+        if path is not None:
+            paths = [path]
+
+        for filename in paths:
+            if os.path.isfile(filename):
+                with open(filename) as config_file:
+                    user_cfg = toml.load(config_file)
+                    cfg.__dict__.update(user_cfg)
+                    cfg.path = config_file
+                    break
+
+        return cfg
+
+    def load_list(self, override_config=None):
+        """Return the loaded list from this config."""
+        if self.list_impl is not None:
+            return self.list_impl
+
+        list_cfg = self.list if override_config is None else override_config
+        try:
+            impl = get_list(list_cfg['name'])
+            if impl is None:
+                print('unknown list: {}'.format(self.list['name']))
+                sys.exit(1)
+        except KeyError:
+            print('no list name provided by config: {}',
+                  toml.dumps(self.__dict__))
+            sys.exit(1)
+
+        try:
+            return impl(**list_cfg['config'])
+        except InvalidConfigError as invalid_config:
+            print('Invalid config: {}'.format(invalid_config))
+            sys.exit(1)
+        except TypeError as unknown_key:
+            print('Invalid config unknown config key: {}'.format(unknown_key))
+            sys.exit(1)
+
+    def toml(self):
+        """Return a toml string of this config."""
+        return toml.dumps({
+            key: val for key, val in self.__dict__.items() if key[0] != '_'
+        })
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
--- src/task_forge/cli/utils.py
+++ src/task_forge/cli/utils.py
@@ -1,94 +1,24 @@
 """Decorators and configuration file loading for the CLI."""
 
 import logging
-import os
 import socket
 import subprocess
+import os
 import sys
+import time
 
-import toml
-
-from task_forge.lists import InvalidConfigError
-from task_forge.lists.load import get_list
-
-CONFIG_FILES = [
-    'taskforge.toml',
-    os.path.join(os.getenv('HOME', ''), '.taskforge.d', 'config.toml'),
-    '/etc/taskforge/config.toml'
-]
-
-
-def default_config():
-    """Return a dict with the default config values."""
-    return {
-        'general': {},
-        'list': {
-            'name': 'sqlite',
-            'config': {
-                'directory': '~/.taskforge.d'
-            }
-        },
-        'server': default_server_config()
-    }
-
-
-def default_server_config():
-    """Return the default configuration for a server."""
-    if sys.platform == 'win32':
-        return {
-            'host': 'localhost',
-            'port': 8080,
-            # 'secret_file': '~/.taskforge.d/server_secret'
-        }
-
-    return {
-        # By default use a unix socket not a network socket
-        'unix_socket': '/var/run/user/{}/taskforge.sock'.format(os.getuid()),
-        # Only used with network communication
-        # 'secret_file': '~/.taskforge.d/server_secret'
-    }
-
-
-def load_config():
-    """Load the config file from the default locations."""
-    cfg = {}
-
-    for filename in CONFIG_FILES:
-        if os.path.isfile(filename):
-            with open(filename) as config_file:
-                cfg = toml.load(config_file)
-
-    default = default_config()
-    default.update(cfg)
-    return default
+from task_forge.cli.config import Config
 
 
 def config(func):
     """Load config and inject it as the keyword argument cfg."""
-    cfg = load_config()
+    cfg = Config.load()
 
     def wrapper(*args, **kwargs):
         kwargs['cfg'] = cfg
         return func(*args, **kwargs)
 
     return wrapper
-
-
-def load_list(cfg):
-    """Load the correct List implementation based on the provided config."""
-    impl = get_list(cfg['list']['name'])
-    if impl is None:
-        print('unknown list: {}'.format(cfg['list']['name']))
-        sys.exit(1)
-
-    try:
-        return impl(**cfg['list']['config'])
-    except InvalidConfigError as invalid_config:
-        print('Invalid config: {}'.format(invalid_config))
-        sys.exit(1)
-    except TypeError as unknown_key:
-        print('Invalid config unknown config key: {}'.format(unknown_key))
-        sys.exit(1)
 
 
 def server_is_reachable(srv_cfg):
@@ -102,12 +32,18 @@ def server_is_reachable(srv_cfg):
 
     try:
         if 'unix_socket' in srv_cfg:
+            logging.debug('checking for unix socket: %s', srv_cfg['unix_socket'])
+            # if not os.path.isfile(srv_cfg['unix_socket']):
+            #     logging.debug('unix socket not found: %s', srv_cfg['unix_socket'])
+            #     return False
             sock.connect(srv_cfg['unix_socket'])
         else:
+            logging.debug('checking for network connection: %s:%d', host, port)
             sock.connect((host, port))
 
         return True
-    except ConnectionRefusedError:
+    except (ConnectionRefusedError, FileNotFoundError):
+        logging.debug('server refused connection')
         return False
     finally:
         sock.close()
@@ -116,25 +52,29 @@ def server_is_reachable(srv_cfg):
 def start_server(cfg):
     """Start the taskforge server as a daemon if necessary."""
     logging.debug('checking if server is running...')
-    srv_cfg = cfg.get('server', {})
-    if server_is_reachable(srv_cfg):
+    if server_is_reachable(cfg.server):
         logging.debug('server is running.')
         return
 
     logging.debug('server is not running')
     logging.debug('starting task server...')
-    subprocess.Popen(['task', 'server'])
-    logging.debug('task server started!')
+    argv = ['taskforged']
+    if cfg.path is not None:
+        argv += ['--config', cfg.path]
+    proc = subprocess.Popen(argv)
+    logging.debug('task server started with pid: %d', proc.pid)
 
     logging.debug('waiting for server to start...')
     retries = 1
-    while not server_is_reachable(srv_cfg) and retries <= 10:
+    while not server_is_reachable(cfg.server) and retries <= 10:
         logging.debug('retrying, attempt: %d', retries)
         retries += 1
+        time.sleep(10)
 
     if retries >= 10:
         logging.debug('retried 10 times, failing to start')
-        raise InvalidConfigError('unable to connect to task server')
+        logging.error('unable to connect to task server')
+        sys.exit(1)
 
 
 def inject_list(func):  # noqa: D202
@@ -143,15 +83,14 @@ def inject_list(func):  # noqa: D202
     @config
     def wrapper(*args, **kwargs):
         cfg = kwargs.pop('cfg')
-        if cfg['general'].get('disable_server', False):
-            kwargs['task_list'] = load_list(cfg)
+        if cfg.general.get('disable_server', False):
+            kwargs['task_list'] = cfg.load_list()
         else:
             start_server(cfg)
-            kwargs['task_list'] = load_list(
-                {'list': {
-                    'name': 'task_server',
-                    'config': cfg['server']
-                }})
+            kwargs['task_list'] = cfg.load_list(override_config={
+                'name': 'task_server',
+                'config': cfg.server,
+            })
 
         return func(*args, **kwargs)
 
