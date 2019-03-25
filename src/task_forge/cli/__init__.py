4c7e5b87a6f08b9b39de7a9ee53d0c0bf59f648d
Mathew Robinson <chasinglogic@gmail.com>
2019-03-25 16:43:28 -0400

    Remove dynamic module importing for subcommands
    This prevents malicious injection of subcommands as well as providing
    a 50 - 60% speed up in command execution.

--- src/task_forge/cli/__init__.py
+++ src/task_forge/cli/__init__.py
@@ -23,7 +23,6 @@ See 'task help <command>' for more information on a specific command.
 
 import logging
 import sys
-from importlib import import_module
 
 from docopt import docopt
 
@@ -67,33 +66,52 @@ def main():
         sys.exit(1)
 
     command = args['<command>']
-    try:
-        if command == 'help' or args['--help']:
-            if args['<args>']:
-                topic = args['<args>'][0]
-            else:
-                topic = None
+    if command == 'help' or args['--help']:
+        if args['<args>']:
+            topic = args['<args>'][0]
+        else:
+            topic = None
 
-            if topic == 'lists':
-                print_lists()
-            elif topic is not None:
+        if topic == 'lists':
+            print_lists()
+        elif topic is not None:
+            try:
+                from importlib import import_module
                 command_mod = import_module(f'task_forge.cli.{topic}_cmd')
                 print(command_mod.__doc__)
-            else:
-                print(__doc__)
-            sys.exit(0)
-
-        if args['--verbose']:
-            logging.basicConfig(level=logging.DEBUG)
-
-        command = ALIASES.get(command, command)
-        command_mod = import_module(f'task_forge.cli.{command}_cmd')
-        argv = [command] + args['<args>']
-        command_mod.run(docopt(command_mod.__doc__, argv=argv))
+            except ImportError:
+                print(f'{topic} is not a known task command')
+                sys.exit(1)
+        else:
+            print(__doc__)
         sys.exit(0)
-    except ImportError:
-        print(f'{command} is not a known task command.')
+
+    if args['--verbose']:
+        logging.basicConfig(level=logging.DEBUG)
+
+    command = ALIASES.get(command, command)
+    if command == 'add':
+        import task_forge.cli.add_cmd as command_mod
+    elif command == 'next':
+        import task_forge.cli.next_cmd as command_mod
+    elif command == 'todo':
+        import task_forge.cli.todo_cmd as command_mod
+    elif command == 'edit':
+        import task_forge.cli.edit_cmd as command_mod
+    elif command == 'complete':
+        import task_forge.cli.complete_cmd as command_mod
+    elif command == 'query':
+        import task_forge.cli.query_cmd as command_mod
+    elif command == 'workon':
+        import task_forge.cli.workon_cmd as command_mod
+    else:
+        print(f'{command} is not a known task command')
+        print(__doc__)
         sys.exit(1)
+
+    argv = [command] + args['<args>']
+    command_mod.run(docopt(command_mod.__doc__, argv=argv))
+    sys.exit(0)
 
 
 if __name__ == '__main__':
