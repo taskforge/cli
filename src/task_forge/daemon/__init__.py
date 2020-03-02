fc6621547a778ce7292cda86d0a84f3cff93ccbe
Mathew Robinson <mathew@chasinglogic.io>
2020-03-02 16:55:01 -0500

    Start daemon implementation

--- src/task_forge/daemon/__init__.py
+++ src/task_forge/daemon/__init__.py
@@ -1,1 +1,11 @@
 """Module containing the implementation of the Taskforge API server."""
+
+from sanic import Sanic
+from sanic.views import HTTPMethodView
+
+app = Sanic()
+
+
+class TaskView(HTTPMethodView):
+
+    async def 
