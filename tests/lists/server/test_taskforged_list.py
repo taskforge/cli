744722f5edef8e41a1ec91f0275422f53286546e
Mathew Robinson <mathew@chasinglogic.io>
2020-03-04 20:12:27 -0500

    Write tests for the TaskforgeDaemonListTests

--- tests/lists/server/test_taskforged_list.py
+++ tests/lists/server/test_taskforged_list.py
@@ -1,29 +1,60 @@
+import os
 import unittest
-from multiprocessing import Process
+import socket
+import requests
+import subprocess
+import pytest
+
+from contextlib import closing
 from tempfile import NamedTemporaryFile
 
+from task_forge.daemon import Daemon
 from task_forge.lists.sqlite import List as SQLiteList
-from task_forge.lists.task_server_client import List
-from task_forge.server.server import Server
+from task_forge.lists.taskforged import List
 
 from ..list_utils import ListTests
 
 
-class TaskforgeServerListTests(unittest.TestCase, ListTests):
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("localhost", 0))
+        return s.getsockname()[1]
+
+
+class TaskforgeDaemonListTests(unittest.TestCase, ListTests):
     def setUp(self):
         self.tmpfile = NamedTemporaryFile()
-        tmp = NamedTemporaryFile(suffix=".sock")
-        self.addr = tmp.name
-        tmp.close()
-        self.server = Server(
-            SQLiteList(file_name=self.tmpfile.name, create_tables=True),
-            unix_socket=self.addr,
+        os.remove(self.tmpfile.name)
+        self.cfgfile = "/tmp/taskforged_test_config.toml"
+
+        port = find_free_port()
+
+        with open(self.cfgfile, "w") as cfgfile:
+            cfgfile.write(
+                """[list]
+name = 'sqlite'
+
+[list.config]
+file_name = '{sqlite_file}'
+
+[server]
+host = 'localhost'
+port = {free_port}
+""".format(
+                    sqlite_file=self.tmpfile.name, free_port=port
+                )
+            )
+
+        self.proc = subprocess.Popen(
+            ["taskforged", "--config-file={}".format(self.cfgfile)]
         )
-        self.proc = Process(target=self.server.run)
-        self.proc.start()
-        self.list = List(unix_socket=self.addr)
+        try:
+            self.proc.communicate(timeout=2)
+        except subprocess.TimeoutExpired:
+            pass
+
+        self.list = List(port=port)
 
     def tearDown(self):
         self.tmpfile.close()
         self.proc.terminate()
-        self.list.client.stop()
