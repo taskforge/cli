7721525e462a1a4cd2d3aa0f14321ecd3252b917
Mathew Robinson <mathew@chasinglogic.io>
2020-05-22 13:50:50 +0100

    Add a get_client utility for acquiring an API client

--- src/task_forge/cli/utils.py
+++ src/task_forge/cli/utils.py
@@ -8,6 +8,7 @@ import time
 from typing import Any, Dict
 
 from task_forge.cli.config import Config
+from task_forge.sdk.client import v1
 
 
 def config(func: Any) -> Any:
@@ -21,3 +22,8 @@ def config(func: Any) -> Any:
     return wrapper
 
 
+def get_client(cfg: Config):
+    """
+    Return the API client for the current API version
+    """
+    return v1.API(cfg.server.hostname)
