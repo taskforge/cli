4914fe7a7132736f977b479bbc17ed18f85e08b0
Mathew Robinson <mathew@chasinglogic.io>
2020-05-25 11:42:39 +0100

    Write tests for the complete command

---
+++ tests/test_complete_cmd.py
@@ -0,0 +1,19 @@
+from tests.utils import CLITestCase
+from task_forge.cli.complete_cmd import run
+from task_forge.sdk.types import Task
+
+
+class TestComplete(CLITestCase):
+    def test_complete_cmd(self):
+        task = Task(id="test", title="test", priority=1)
+        args = self.args()
+        args["<ID>"] = [task.id]
+        run(args)
+        self.client.tasks.complete_by_id.assert_called_with(task.id)
+
+    def test_completes_current_with_no_ids(self):
+        task = Task(id="test", title="test", priority=1)
+        self.client.tasks.current.return_value = task
+        args = self.args()
+        run(args)
+        self.client.tasks.complete_by_id.assert_called_with(task.id)
