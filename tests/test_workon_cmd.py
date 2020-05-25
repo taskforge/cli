1c170a3bf9a8d0cef651fdf0f211b22ee8f5a096
Mathew Robinson <mathew@chasinglogic.io>
2020-05-25 11:42:23 +0100

    Write tests for the Workon command

---
+++ tests/test_workon_cmd.py
@@ -0,0 +1,15 @@
+from tests.utils import CLITestCase
+from task_forge.cli.workon_cmd import run
+from task_forge.sdk.types import Task
+
+
+class TestWorkon(CLITestCase):
+    def test_workon_cmd(self):
+        task = Task(id="test", title="test", priority=1)
+        self.client.tasks.current.return_value = Task(title="test", priority=100)
+        self.client.tasks.get.return_value = task
+        args = self.args()
+        args["<ID>"] = task.id
+        run(args)
+        self.client.tasks.get.assert_called_with(task.id)
+        self.assertEqual(task.priority, 101)
