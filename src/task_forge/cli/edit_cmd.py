5d24e49fa4dec45e4e25018355326f4a302c1a2e
Mathew Robinson <mathew@chasinglogic.io>
2020-05-25 11:43:36 +0100

    Re-implement the edit command
    This now uses yaml which is a bit easier to deal with when it comes to
    large text bodies. Still doesn't support comments

--- src/task_forge/cli/edit_cmd.py
+++ src/task_forge/cli/edit_cmd.py
@@ -1,7 +1,7 @@
 """
 Usage: task edit [<ID>]
 
-Edit the task indicated by ID as a toml file. If no ID given opens the current
+Edit the task indicated by ID as a yaml file. If no ID given opens the current
 task.
 
 Will use $EDITOR if set and if not will attempt to find an editor based on
@@ -10,56 +10,64 @@ platform.
 
 import os
 import sys
-import shlex
-from typing import Any, Dict, cast
 from tempfile import NamedTemporaryFile
 from subprocess import call
 
-import toml
+import yaml
 
-from task_forge.lists import TaskList, NotFoundError
-from task_forge.models import Task
-from task_forge.cli.utils import inject_list
+from task_forge.cli.utils import config, get_client
+from task_forge.sdk.types import Task
+from task_forge.sdk.exceptions import NotFound
 
 
-def get_editor_program() -> str:
+def get_editor_program():
     """Return editor based on operating system"""
-    if sys.platform == "win32":
-        if "EDITOR" in os.environ:
-            return os.getenv("EDITOR")
+    editor_prg = os.getenv("EDITOR", None)
+    if editor_prg is not None:
+        return editor
 
+    if sys.platform == "win32":
         return "notepad.exe"
 
-    return os.getenv("EDITOR", "vi")
+    return "vi"
 
 
-def editor(filename: str) -> None:
+def editor(filename):
     """Open filename in $EDITOR"""
     program = get_editor_program()
-    args = f"{program} {filename}"
-    call(shlex.split(args), stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+    call([program, filename], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
 
 
-@inject_list
-def run(args: Any, task_list: TaskList) -> None:
+@config
+def run(args, cfg):
     """Open task by ID in $EDITOR. Update task based on result."""
+    client = get_client(cfg)
+
     try:
         if args["<ID>"]:
-            task = task_list.find_by_id(args["<ID>"])
+            task = client.tasks.get(args["<ID>"])
         else:
-            task = task_list.current()
+            task = client.tasks.current()
 
+        # This slightly awkward dance that looks like it could be solved by a context manager is
+        # caused by a few issues. First NamedTemporaryFile if used as a context manager will auto
+        # delete itself when the block exits. That means that if we use a context manager for it we
+        # can't release the file handle without deleting the file. For Windows we have to release
+        # the file handle to allow $EDITOR to open it (otherwise it will complain about the file
+        # being busy). So what we have to do is open the file for writing, write to it explicitly
+        # close the handle (so it doesn't delete itself), open it with the editor program, then
+        # re-open it for reading ourselves. Finally we have to explicitly clean up the temp file.
         tmp = NamedTemporaryFile(mode="w+", suffix=".toml", delete=False)
-        toml.dump(task.to_dict(), tmp)
+        yaml.dump(task.to_dict(), tmp)
         tmp.close()
 
         editor(tmp.name)
 
         with open(tmp.name) as tmp:
-            new_task = Task.from_dict(cast(Dict[str, Any], toml.load(tmp)))
+            new_task = Task(**yaml.safe_load(tmp))
 
-        task_list.update(new_task)
+        client.tasks.update(new_task)
         os.remove(tmp.name)
-    except NotFoundError:
-        print("no task with that ID exists")
+    except NotFound:
+        print("No task with that ID exists")
         sys.exit(1)
