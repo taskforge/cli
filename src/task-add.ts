9ed7c02182b7c0867ee5d5cd352cbc1ea6188349
Mathew Robinson <mathew@chasinglogic.io>
2020-07-01 22:17:51 +0100

    build the add command

---
+++ src/task-add.ts
@@ -0,0 +1,46 @@
+import { Command } from 'commander';
+
+import { tasks, contexts, isAPIError } from '@taskforge/sdk';
+
+import { fail, highestPriority } from './utils';
+
+async function main() {
+    const cli = new Command();
+    cli.option('-t --top', 'if provided make this task the top priority')
+        .option(
+            '-c --context <name>',
+            'the context in which to create the task',
+            'default'
+        )
+        .arguments('[title...]')
+        .parse(process.argv);
+
+    const title = cli.args.join(' ');
+    if (!title || title === '') {
+        console.log('must provide a task title');
+        process.exit(1);
+    }
+
+    let priority = 1;
+    if (cli.top) {
+        priority = (await highestPriority()) + 1;
+    }
+
+    let context;
+    if (cli.context && cli.context !== 'default') {
+        const contextObj = await contexts.byName(cli.context);
+        if (isAPIError(contextObj)) {
+            fail(contextObj);
+            return;
+        }
+
+        context = contextObj.id;
+    }
+
+    const response = await tasks.create({ title, priority, context });
+    if (isAPIError(response)) {
+        fail(response);
+    }
+}
+
+main();
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,23 @@
 import kleur from 'kleur';
-import { APIError } from '@taskforge/sdk';
+import { APIError, tasks, isAPIError } from '@taskforge/sdk';
 
 export const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export function fail(err: APIError): void {
     console.log(kleur.red('ERROR'), err.message);
     process.exit(err.code);
+}
+
+export async function highestPriority(): Promise<number> {
+    const current = await tasks.current();
+    if (isAPIError(current)) {
+        if (current.code === 404) {
+            return 2;
+        }
+
+        fail(current);
+        return 0;
+    }
+
+    return current.priority;
 }
