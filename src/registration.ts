6e50a7d292e121ff096ff1240bfca2f488f1caa5
Mathew Robinson <mathew@chasinglogic.io>
2020-07-05 14:40:20 +0100

    fix: improve error handling
    Before if login or registration threw an exception it would cause the
    cli to hand indefinitely.

--- src/registration.ts
+++ src/registration.ts
@@ -16,50 +16,55 @@ export async function loginOrRegister(register = true) {
         { name: 'password', message: 'Password:', type: 'password' }
     ]);
 
-    if (register) {
-        const { fullName } = await prompts([
-            {
-                name: 'fullName',
-                message: 'Full Name (optional):',
-                type: 'text'
+    try {
+        if (register) {
+            const { fullName } = await prompts([
+                {
+                    name: 'fullName',
+                    message: 'Full Name (optional):',
+                    type: 'text'
+                }
+            ]);
+            const registerSpinner = ora('Creating your account').start();
+            const user = await users.create({ email, password, fullName });
+            if (isAPIError(user)) {
+                fail(user);
+                return;
             }
-        ]);
-        const registerSpinner = ora('Creating your account').start();
-        const user = await users.create({ email, password, fullName });
-        if (isAPIError(user)) {
-            fail(user);
+
+            registerSpinner.stopAndPersist({
+                text: kleur.bold('Registered successfully!'),
+                prefixText: kleur.green('✔')
+            });
+        }
+
+        const spinner = ora('Generating a personal access token').start();
+        const pat = await users.generatePat({ email, password });
+        if (isAPIError(pat)) {
+            fail(pat);
             return;
         }
 
-        registerSpinner.stopAndPersist({
-            text: kleur.bold('Registered successfully!'),
+        spinner.stopAndPersist({
+            text: kleur.bold('Generated token'),
             prefixText: kleur.green('✔')
         });
+
+        console.log('Your personal access token is:');
+        console.log('\n', pat.pat, '\n');
+        console.log(
+            'This token must be set as the environment variable' +
+                ' TASKFORGE_TOKEN for use with this CLI. Add this to your shell' +
+                ' configuration file to use it:'
+        );
+        console.log('\n', `export TASKFORGE_TOKEN='${pat.pat}'`, '\n');
+        console.log(
+            kleur.yellow('WARNING!'),
+            'This personal access token can be revoked but it gives full permissions' +
+                ' to operate on taskforge as your user. Protect it like a password.'
+        );
+    } catch (e) {
+        console.log('Unexpected error:', e);
+        process.exit(5);
     }
-
-    const spinner = ora('Generating a personal access token').start();
-    const pat = await users.generatePat({ email, password });
-    if (isAPIError(pat)) {
-        fail(pat);
-        return;
-    }
-
-    spinner.stopAndPersist({
-        text: kleur.bold('Generated token'),
-        prefixText: kleur.green('✔')
-    });
-
-    console.log('Your personal access token is:');
-    console.log('\n', pat.pat, '\n');
-    console.log(
-        'This token must be set as the environment variable' +
-            ' TASKFORGE_TOKEN for use with this CLI. Add this to your shell' +
-            ' configuration file to use it:'
-    );
-    console.log('\n', `export TASKFORGE_TOKEN='${pat.pat}'`, '\n');
-    console.log(
-        kleur.yellow('WARNING!'),
-        'This personal access token can be revoked but it gives full permissions' +
-            ' to operate on taskforge as your user. Protect it like a password.'
-    );
 }
