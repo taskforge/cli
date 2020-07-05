704ea20b4c17c326526dc38a963fd354b4b9c6e1
Mathew Robinson <mathew@chasinglogic.io>
2020-07-05 14:41:36 +0100

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ cli.command('add', 'add a task to your list')
     .command('list', 'list your tasks')
     .command('query', 'search your tasks')
     .command('complete', 'complete tasks')
-    .command('show', 'show detailed information about a task, including comments')
+    .command(
+        'show',
+        'show detailed information about a task, including comments'
+    )
     .command('workon', 'make a task the top priority')
     .command('next', 'show your next or "current" task')
     .command('todo', 'list your incomplete tasks')
