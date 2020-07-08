4d2e6ba417acedc0034de99a7dd6899c9708e63d
Mathew Robinson <mathew@chasinglogic.io>
2020-07-08 10:20:46 +0100

    Add a healthcheck script for tests

---
+++ scripts/healthcheck.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+
+async function main() {
+    const url = `${process.env.TASKFORGE_HOST}/v1/tasks`;
+    const params = {
+        method: 'OPTIONS'
+    };
+
+    let attempts = 0;
+    while (attempts < 10) {
+        console.log(`Attempt #${attempts} to connect to ${url}`);
+        const response = await fetch(url, params);
+        if (response.status === 200) {
+            console.log('API is ready!');
+            process.exit(0);
+        }
+
+        attempts++;
+    }
+
+    console.log(`Failed to connect after ${attempts} attempts.`);
+    process.exit(1);
+}
+
+main();
