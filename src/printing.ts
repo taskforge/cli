0ee58ac559ea1bd8bcf213511b9801ef07792dbf
Mathew Robinson <mathew@chasinglogic.io>
2020-07-08 06:29:24 +0100

    feature: humanize output of tasks
    Before we were printing raw IDs for various fields that represented
    other objects on the task. For instance Context, Source, and Owner. Now
    we make the appropriate calls so we can print more human-friendly values
    such as the Context's name or the user's fullName / email.

--- src/printing.ts
+++ src/printing.ts
@@ -1,8 +1,73 @@
-import { tasks, Task, isAPIError } from '@taskforge/sdk';
+import { users, sources, contexts, tasks, Task, isAPIError } from '@taskforge/sdk';
 import { fail } from './utils';
 
-export function printTable(list: Task[]): void {
-    console.table(list);
+function idMemo<T>(fn: (id: string) => Promise<T>): (id: string) => Promise<T> {
+    const memo: { [id: string]: T } = {};
+    return async (id: string): Promise<T> => {
+        if (memo[id]) {
+            return memo[id];
+        }
+
+        const model = await fn(id);
+        return model;
+    };
+}
+
+const getContext = idMemo(contexts.get);
+const getSource = idMemo(sources.get);
+const getUser = idMemo(users.get);
+
+async function humanize(task: Task): Promise<any> {
+    const context = await getContext(task.context);
+    if (isAPIError(context)) {
+        console.log(
+            'Unexpected error retrieving context:',
+            context.code,
+            context.message
+        );
+        process.exit(1);
+    }
+
+    const source = await getSource(task.source);
+    if (isAPIError(source)) {
+        console.log(
+            'Unexpected error retrieving source:',
+            source.code,
+            source.message
+        );
+        process.exit(1);
+    }
+
+    const owner = await getUser(task.owner);
+    if (isAPIError(owner)) {
+        console.log(
+            'Unexpected error retrieving owner:',
+            owner.code,
+            owner.message
+        );
+        process.exit(1);
+    }
+
+    return {
+        ...task,
+        owner: owner.fullName !== '' ? owner.fullName : owner.email,
+        context: context.name,
+        source: source.name
+    };
+}
+
+export async function printTable(list: Task[]): Promise<void> {
+    const formatted: { [id: string]: any } = {};
+
+    await Promise.all(
+        list.map(async (task) => {
+            // eslint-disable-next-line
+            const { id, ...humanized } = await humanize(task);
+            formatted[id] = humanized;
+        })
+    );
+
+    console.table(formatted);
 }
 
 export function printJSON(list: any): void {
@@ -13,14 +78,14 @@ export function printList(list: Task[], format: string): void {
     switch (format) {
         case 'json':
             printJSON(list);
-            break;
+        break;
         default:
             if (list.length === 0) {
-                console.log('No tasks to show!');
-                return;
-            }
+            console.log('No tasks to show!');
+            return;
+        }
 
-            printTable(list);
+        printTable(list);
     }
 }
 
@@ -38,25 +103,6 @@ export async function highestPriority(): Promise<number> {
     return current.priority;
 }
 
-export function printTask(task: Task): void {
-    const table = new Table({
-        title: task.title
-    });
-
-    function prop<K extends keyof Task>(obj: Task, key: K) {
-        return obj[key];
-    }
-
-    table.addRows(
-        Object.keys(task)
-            .filter((key) => key !== 'title')
-            .map((key) => {
-                return {
-                    field: key,
-                    value: prop(task, key as keyof Task)
-                };
-            })
-    );
-
-    table.printTable();
+export async function printTask(task: Task): Promise<void> {
+    console.table(await humanize(task));
 }
