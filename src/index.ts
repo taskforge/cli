b4f65ed4556f51b801b58d6c9ac380b48b326e6d
Mathew Robinson <mathew@chasinglogic.io>
2020-07-09 11:30:19 +0100

    feature: add convenience aliases for all commands

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,25 @@ const cli = new Command();
 
 cli.command('add', 'add a task to your list')
     .command('list', 'list your tasks')
+    .alias('l')
+    .alias('ls')
     .command('query', 'search your tasks')
+    .alias('q')
+    .alias('search')
     .command('complete', 'complete tasks')
+    .alias('done')
+    .alias('c')
     .command(
         'show',
         'show detailed information about a task, including comments'
     )
+    .alias('s')
     .command('workon', 'make a task the top priority')
+    .alias('w')
     .command('next', 'show your next or "current" task')
+    .alias('n')
     .command('todo', 'list your incomplete tasks')
+    .alias('td')
     .command('register', 'register a new account with the taskforge server')
     .command('login', 'generate a personal access token')
     .version(version)
