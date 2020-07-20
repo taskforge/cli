f5bc6dbcd9b025e013d2a21d0e6a5a2b18312a84
Mathew Robinson <mathew@chasinglogic.io>
2020-07-20 08:40:12 +0100

    write tests for the complete command

---
+++ tests/test-complete-cmd.ts
@@ -0,0 +1,59 @@
+import { cli, listTasks, generateTask, generateUser } from './utils';
+
+describe('task complete', () => {
+    test('no arguments completes the current task', async () => {
+        const { token } = await generateUser();
+        await generateTask(token);
+        const { id } = await generateTask(token, { priority: 2 });
+        await cli('complete', token);
+        const tasks = await listTasks(token);
+        for (const task of tasks) {
+            if (task.id === id) {
+                expect(task.completedDate).not.toBe(null);
+            } else {
+                expect(task.completedDate).toBe(null);
+            }
+        }
+    });
+
+    test('given an id completes the indicated task', async () => {
+        const { token } = await generateUser();
+        const { id } = await generateTask(token);
+        await generateTask(token, { priority: 2 });
+        await cli(`complete ${id}`, token);
+        const tasks = await listTasks(token);
+        for (const task of tasks) {
+            if (task.id === id) {
+                expect(task.completedDate).not.toBe(null);
+            } else {
+                expect(task.completedDate).toBe(null);
+            }
+        }
+    });
+
+    test('accepts multiple ids and completes the indicated tasks', async () => {
+        const { token } = await generateUser();
+        const { id } = await generateTask(token);
+        const task2 = await generateTask(token, { priority: 2 });
+        await cli(`complete ${id} ${task2.id}`, token);
+        const tasks = await listTasks(token);
+        for (const task of tasks) {
+            expect(task.completedDate).not.toBe(null);
+        }
+    });
+
+    test('done is an alias to complete', async () => {
+        const { token } = await generateUser();
+        await generateTask(token);
+        const { id } = await generateTask(token, { priority: 2 });
+        await cli('done', token);
+        const tasks = await listTasks(token);
+        for (const task of tasks) {
+            if (task.id === id) {
+                expect(task.completedDate).not.toBe(null);
+            } else {
+                expect(task.completedDate).toBe(null);
+            }
+        }
+    });
+});
