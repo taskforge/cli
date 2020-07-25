ce33ee58e9f02685f1deaefc5fa4785b280ff785
Mathew Robinson <mathew@chasinglogic.io>
2020-07-25 11:55:57 +0100

    write list command tests, remove unused bin/task-query

---
+++ tests/test-list-cmd.ts
@@ -0,0 +1,40 @@
+import { cli, generateTask, generateUser } from './utils';
+
+describe('task list', () => {
+    test('--output json returns valid json', async () => {
+        const { token } = await generateUser();
+        const task = await generateTask(token);
+        const { stdout } = await cli('list -o json', token);
+        const parsed = JSON.parse(stdout);
+        expect(parsed).toStrictEqual([task]);
+    });
+
+    test('lists all tasks', async () => {
+        const { token } = await generateUser();
+        const task1 = await generateTask(token);
+        const task2 = await generateTask(token);
+        const task3 = await generateTask(token, {}, true);
+        const { stdout } = await cli('list -o json', token);
+        const parsed = JSON.parse(stdout);
+        expect(parsed).toStrictEqual([task1, task2, task3]);
+    });
+
+    test('list accepts TQL queries', async () => {
+        const { token } = await generateUser();
+        const task1 = await generateTask(token);
+        await generateTask(token, null, true);
+        const expected = [task1];
+        await generateTask(token, {}, true);
+        const { stdout } = await cli('list -o json completed = false', token);
+        const parsed = JSON.parse(stdout);
+        expect(parsed).toStrictEqual(expected);
+    });
+
+    test.each(['q', 'query', 'l'])('%s is an alias for list', async (alias) => {
+        const { token } = await generateUser();
+        const task = await generateTask(token);
+        const { stdout } = await cli(`${alias} -o json`, token);
+        const parsed = JSON.parse(stdout);
+        expect(parsed).toStrictEqual([task]);
+    });
+});
