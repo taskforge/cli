39453d0e9b805b33400751965fb6ce75a896611d
Mathew Robinson <mathew@chasinglogic.io>
2020-07-25 21:32:36 +0100

    fix: current should be an alias for next

--- tests/test-next-cmd.ts
+++ tests/test-next-cmd.ts
@@ -39,4 +39,14 @@ describe('task next', () => {
         const { stdout } = await cli('next --json', token);
         expect(JSON.parse(stdout)).toStrictEqual(currentTask);
     });
+
+    test('task current is an alias for task next', async () => {
+        const { token } = await generateUser();
+        await generateTask(token);
+        await generateTask(token);
+        const currentTask = await generateTask(token, { priority: 2 });
+        const { stdout } = await cli('current', token);
+        const rgx = new RegExp(`^${currentTask.id} ${currentTask.title}`);
+        expect(stdout).toMatch(rgx);
+    });
 });
