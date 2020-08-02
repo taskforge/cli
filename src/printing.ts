7cfcc5022199d164d212f08c8f5f72845cacf614
Mathew Robinson <mathew@chasinglogic.io>
2020-08-02 10:46:54 +0100

    refactor: remove dead function

--- src/printing.ts
+++ src/printing.ts
@@ -117,20 +117,6 @@ export function printList(list: Task[], format: string): void {
     }
 }
 
-export async function highestPriority(): Promise<number> {
-    const current = await tasks.current();
-    if (isAPIError(current)) {
-        if (current.code === 404) {
-            return 2;
-        }
-
-        fail(current);
-        return 0;
-    }
-
-    return current.priority;
-}
-
 export async function printTask(task: Task): Promise<void> {
     const humanized = await humanize(task);
     const data: any[][] = Object.keys(humanized).map((key) => {
