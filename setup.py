5d24004c7b316bc1bcb918f4b3551eee9e34c4ba
Mathew Robinson <mathew@chasinglogic.io>
2021-03-31 21:08:10 +0100

    init

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""A task management tool that integrates with 3rd party services."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+with open("requirements.txt") as f:
+    install_requires = [line for line in f if line]
+
+setup(
+    name="taskforge-cli",
+    version="0.3.0",
+    url="https://github.com/taskforge/cli",
+    license="Apache-2.0",
+    author="Mathew Robinson",
+    author_email="chasinglogic@gmail.com",
+    description="A task management tool that automates your workflow",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=True,
+    platforms="any",
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["task = taskforge:cli"]},
+    classifiers=[
+        # As from https://pypi.org/classifiers/
+        "Development Status :: 4 - Beta",
+        # 'Development Status :: 5 - Production/Stable',
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
