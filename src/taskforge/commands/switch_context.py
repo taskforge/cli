4f150223b2b5509818c23a5cf3dc84e19fc27289
Mathew Robinson <mathew@chasinglogic.io>
2021-04-02 22:42:39 +0100

    fix: Allow switching back to no context

--- src/taskforge/commands/switch_context.py
+++ src/taskforge/commands/switch_context.py
@@ -5,7 +5,7 @@ from taskforge.state import State
 
 
 @cli.command(aliases=["sc"])
-@click.argument("context")
+@click.argument("context", nargs=1, required=False)
 def switch_context(context):
     """
     Switch the current context.
@@ -13,5 +13,8 @@ def switch_context(context):
     This will change what the next task is such that the only tasks considered are of
     the given context. Useful when you're leaving work and coming home, for instance.
     """
-    State.current_context = context
+    if context:
+        State.current_context = context[0]
+    else:
+        State.current_context = None
     State.save()
